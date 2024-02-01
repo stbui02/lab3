4f3ba4e2b13dcddbaf3a6919c35fbb38b3e4c90e
stbui02 <stbui@ucsd.edu>
2024-02-01 11:09:04 -0800

    Update to ArrayTests and ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,20 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 3, 2, 1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 1, 2, 3 }, input1);
+	}
+  
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 2 };
+    assertArrayEquals(new int[]{ 2 }, ArrayExamples.reversed(input1));
+  }
 }
+
+
